25f05f9d4120c908aaea5b6d2bc7a781192dcf45
Michael Kantor <empathynyc@gmail.com>
2017-03-14 17:40:36 +0000

    OrganizationRepository.php edited online with Bitbucket

--- src/Repositories/OrganizationRepository.php
+++ src/Repositories/OrganizationRepository.php
@@ -1,40 +1,39 @@
 <?php namespace OLC\AIMSUserDriver\Repositories;
 
-use Drapor\Networking\Networking;
-use OLC\AIMSUserDriver\Models\User;
-use OLC\AIMSUserDriver\Models\Membership;
 use OLC\AIMSUserDriver\Models\Organization;
-use OLC\AIMSUserDriver\Models\OrganizationMembership;
-use OLC\AIMSUserDriver\Models\MembershipType;
 use OLC\AIMSUserDriver\Services\AIMSService;
 
 class OrganizationRepository
 {
-	protected $service;
+    protected $service;
 
-	public function __construct(AIMSService $AIMSService)
-	{
-		$this->service  = $AIMSService;
-	}
+    public function __construct(AIMSService $AIMSService)
+    {
+        $this->service = $AIMSService;
+    }
 
-	public function map($organizationResponse)
-	{
-		if($organizationResponse != null)
-		{
-			$organization = new Organization($organizationResponse);
-		}
-		return $organization;
-	}
+    public function map($organizationResponse)
+    {
+        if ($organizationResponse != null)
+        {
+            $organization = new Organization($organizationResponse);
+            if (isset($organization->children))
+            {
+                $children               = collect($organization->children);
+                $organization->children = $children;
+            }
+        }
+        return $organization;
+    }
 
-	public function lists($type)
-	{
-		return $this->service->organizationsBy($type);
-	}
+    public function lists($type)
+    {
+        return $this->service->organizationsBy($type);
+    }
 
-	public function find($id)
-	{
-		return $this->map($this->service->getOrganization($id));
-	}
-
+    public function find($id)
+    {
+        return $this->map($this->service->getOrganization($id));
+    }
 
 }
