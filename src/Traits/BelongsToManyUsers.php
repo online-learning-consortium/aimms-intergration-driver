efccc4e2fbc3ca12c18451ac1dd17485aa256a98
Michael Kantor <empathynyc@gmail.com>
2017-07-25 20:35:54 +0000

    BelongsToManyUsers.php edited online with Bitbucket

--- src/Traits/BelongsToManyUsers.php
+++ src/Traits/BelongsToManyUsers.php
@@ -1,21 +1,27 @@
 <?php namespace OLC\AIMSUserDriver\Traits;
 
- use OLC\AIMSUserDriver\Repositories\UserRepository;
- use OLC\AIMSUserDriver\Models\Relations\ManyHttpRelations;
- use Illuminate\Database\Eloquent\Builder;
- use OLC\AIMSUserDriver\Models\DummyModel;
- use DB;
+use Illuminate\Database\Eloquent\Builder;
+use OLC\AIMSUserDriver\Models\Relations\ManyHttpRelations;
 
- /*
-    If there's something that needs acceptance testing it is probally this 
-  */
- trait BelongsToManyUsers
- {
- 	public $btmUsersTable;
+/*
+If there's something that needs acceptance testing it is probally this
+ */
+trait BelongsToManyUsers
+{
+    public $btmUsersTable;
 
- 	public function users()
- 	{
- 		$builder    =  app(Builder::class);
- 		return new ManyHttpRelations($builder,$this);
- 	}
+    public function users()
+    {
+        $builder = app(Builder::class);
+        return new ManyHttpRelations($builder, $this);
+    }
+
+    public function getUsersAttribute()
+    {
+        if (isset($this->attributes['users']))
+        {
+            return $this->attributes['users'];
+        }
+        return collect();
+    }
 }
